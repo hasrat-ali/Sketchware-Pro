814def54a48ffb5e382e958c7b4b927094dd436a
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-01-18 00:33:39 +0530

    feat(wip): Update BuiltInLibraries to their latest (compatible) versions

--- app/src/main/java/mod/agus/jcoderz/editor/library/ExtLibSelected.java
+++ app/src/main/java/mod/agus/jcoderz/editor/library/ExtLibSelected.java
@@ -21,16 +21,16 @@ public class ExtLibSelected {
             kp.addLibrary(BuiltInLibraries.PLAY_SERVICES_GCM);
         }
         if (component.isFBAdsUsed) {
-            kp.addLibrary(BuiltInLibraries.FACEBOOK_ADS_AUDIENCE_NETWORK_SDK);
+            kp.addLibrary(BuiltInLibraries.AUDIENCE_NETWORK_SDK);
         }
         if (component.isLottieUsed) {
             kp.addLibrary(BuiltInLibraries.LOTTIE);
         }
         if (component.isYoutubePlayerUsed) {
-            kp.addLibrary(BuiltInLibraries.YOUTUBE_PLAYER);
+            kp.addLibrary(BuiltInLibraries.ANDROID_YOUTUBE_PLAYER);
         }
         if (component.isCircleImageViewUsed) {
-            kp.addLibrary(BuiltInLibraries.CIRCLE_IMAGEVIEW);
+            kp.addLibrary(BuiltInLibraries.CIRCLEIMAGEVIEW);
         }
         if (component.isFBGoogleUsed) {
             kp.addLibrary(BuiltInLibraries.PLAY_SERVICES_AUTH);
@@ -39,7 +39,7 @@ public class ExtLibSelected {
             kp.addLibrary(BuiltInLibraries.OTPVIEW);
         }
         if (component.isCodeViewUsed) {
-            kp.addLibrary(BuiltInLibraries.CODE_VIEW);
+            kp.addLibrary(BuiltInLibraries.CODEVIEW);
         }
         if (component.isPatternLockViewUsed) {
             kp.addLibrary(BuiltInLibraries.PATTERN_LOCK_VIEW);
